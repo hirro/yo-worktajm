3141b0b0b6b2840d47c167dc2f055696ebdcd5ea
Jim Arnell <jim@arnellconsulting.com>
2013-11-16 23:47:16 +0100

    no message

--- karma.conf.js
+++ karma.conf.js
@@ -3,17 +3,21 @@
 
 module.exports = function(config) {
   config.set({
+    // base path, that will be used to resolve files and exclude
     basePath: '',
+
+    // testing framework to use (jasmine/mocha/qunit/...)
     frameworks: ['jasmine'],
-    browsers: ['PhantomJS'],    
+
+    // list of files / patterns to load in the browser
     files: [
       'app/bower_components/angular/angular.js',
-      'app/bower_components/jquery/jquery.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/bower_components/angular-resource/angular-resource.js',
       'app/bower_components/angular-cookies/angular-cookies.js',
       'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/jquery/jquery.js',
       'app/bower_components/restangular/dist/restangular.js',
       'app/bower_components/lodash/lodash.js',
       'app/bower_components/angular-webstorage/angular-webstorage.js',
@@ -24,31 +28,36 @@ module.exports = function(config) {
       'test/mock/**/*.js',
       'test/spec/**/*.js'
     ],
-    captureTimeout: 60000,
-    colors: true,
-    exclude: ['dist/'],
+
+    // list of files / patterns to exclude
+    exclude: [],
+
+    // web server port
+    port: 8080,
 
     // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_DEBUG,
-    port: 9876,
-    plugins: [
-      'karma-chrome-launcher',
-      'karma-firefox-launcher',
-      'karma-phantomjs-launcher',
-      'karma-jasmine',
-      'karma-coverage'
-    ],
-    runnerPort: 9100,
-    singleRun: true,
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
-    coverageReporter: {
-      type: 'lcov',
-      dir: 'coverage/'
-    },
-    preprocessors: {
-      'src/angular-cache.js': ['coverage']
-    },
-    reporters: ['progress', 'coverage']
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: true
   });
 };
+
