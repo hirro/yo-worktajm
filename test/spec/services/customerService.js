a9f06a3a03ba2555b0f28d079098888e3d4da000
Jim Arnell <jim@arnellconsulting.com>
2013-12-25 12:08:38 +0100

    TDD create customer.

--- test/spec/services/customerService.js
+++ test/spec/services/customerService.js
@@ -1,3 +1,31 @@
+/*
+  @licstart The following is the entire license notice for the 
+            JavaScript code in this page.
+  @source https://github.com/hirro/yo-worktajm
+
+  Copyright (C) 2013 Jim Arnell.
+
+  The JavaScript code in this page is free software: you can
+  redistribute it and/or modify it under the terms of the GNU
+  General Public License (GNU GPL) as published by the Free Software
+  Foundation, either version 3 of the License, or (at your option)
+  any later version.  The code is distributed WITHOUT ANY WARRANTY;
+  without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+
+  As additional permission under GNU GPL version 3 section 7, you
+  may distribute non-source (e.g., minimized or compacted) forms of
+  that code without the copy of the GNU GPL normally required by
+  section 4, provided you include this license notice and a URL
+  through which recipients can access the Corresponding Source.
+
+  @licend The above is the entire license notice
+          for the JavaScript code in this page.  
+*/
+
+
+/*globals expect, afterEach, beforeEach, spyOn, describe, it, inject, _ */
+
 'use strict';
 
 describe('Service: CustomerService', function () {
@@ -7,148 +35,70 @@ describe('Service: CustomerService', function () {
 
   // instantiate service
   var httpBackend;
-  var CustomerService;
-  var PersonService;
+  var customerService;
+  var personService;
   var scope;
 
   // Test constants
   var customers = [
     { id: 301, name: 'Project A' },
     { id: 302, name: 'Project B' }
-  ];  
+  ];
 
-  beforeEach(inject(function (_PersonService_, _CustomerService_, $httpBackend, $rootScope) {
-    CustomerService = _CustomerService_;
-    PersonService = _PersonService_;    
+  beforeEach(inject(function (PersonService, CustomerService, $httpBackend, $rootScope) {
+    customerService = CustomerService;
+    personService = PersonService;
     httpBackend = $httpBackend;
     scope = $rootScope;
 
     // Assume we are logged in as user id 1
-    PersonService.personId = 1;    
+    PersonService.personId = 1;
   }));
 
+  afterEach(function () {
+    httpBackend.verifyNoOutstandingExpectation();
+    httpBackend.verifyNoOutstandingRequest();
+  });
 
   describe('create tests', function () {
     it('should create a new customer', function () {
       // Setup
-      httpBackend.whenDELETE('http://worktajm.arnellconsulting.dyndns.org:8080/api/api/customer').respond(_.clone(customers[0]));
-      spyOn(CustomerService, 'getCustomer').andCallThrough();
+      httpBackend.whenPOST('http://worktajm.arnellconsulting.dyndns.org:8080/api/api/customer').respond(_.clone(customers[0]));
+      spyOn(customerService, 'updateCustomer').andCallThrough();
       spyOn(scope, '$broadcast').andCallThrough();
 
       // Test
-      var result = CustomerService.getCustomer(1);
+      var result;
+      customerService.updateCustomer(customers[0]).then(function (r) {
+        result = r;
+      }, function () {
+      });
       scope.$digest();
       httpBackend.flush();
       
       // Validation
-      expect(CustomerService.getCustomers).toHaveBeenCalledWith(1);
+      expect(customerService.updateCustomer).toHaveBeenCalledWith(customers[0]);
       expect(result.id).toBe(1);
     });
 
     it('should try to create a new customer but get an error from backend', function () {
       // Setup
-      httpBackend.whenDELETE('http://worktajm.arnellconsulting.dyndns.org:8080/api/api/customer').respond(401);
-      spyOn(CustomerService, 'getCustomes').andCallThrough();
+      httpBackend.whenPOST('http://worktajm.arnellconsulting.dyndns.org:8080/api/api/customer').respond(401);
+      spyOn(customerService, 'getCustomer').andCallThrough();
       spyOn(scope, '$broadcast').andCallThrough();
 
       // Test
-      var result = CustomerService.getCustomer(1);
+      var failMessage;
+      customerService.updateCustomer(customers[0]).then(function () {
+      }, function (msg) {
+        failMessage = msg;
+      });
       scope.$digest();
       httpBackend.flush();
       
       // Validation
-      expect(CustomerService.getCustomers).toHaveBeenCalledWith(1);
-      expect(result.id).toBe(1);
-    });
-  });
-
-  xdescribe('list tests', function () {
-    it('should get a list of customers', function () {
-      // Setup
-      httpBackend.whenGET('http://worktajm.arnellconsulting.dyndns.org:8080/api/api/customer').respond(_.clone(customers));
-      spyOn(CustomerService, 'getCustomers').andCallThrough();
-      spyOn(scope, '$broadcast').andCallThrough();
-
-      // Test
-      var result = CustomerService.getCustomers();
-      scope.$digest();
-      httpBackend.flush();
-      
-      // Validation
-      expect(CustomerService.getCustomers).toHaveBeenCalled();
-      expect(result).toBeDefined();
-      expect(result).not.toBeNull();
-      expect(result.length).toBe(1);
-
-    });
-
-    it('should try to get a list of customers but get an error from backend', function () {
-      // Setup
-      httpBackend.whenGET('http://worktajm.arnellconsulting.dyndns.org:8080/api/api/customer').respond(401);
-      spyOn(CustomerService, 'getCustomers').andCallThrough();
-      spyOn(scope, '$broadcast').andCallThrough();
-
-      // Test
-      var result = CustomerService.getCustomers();
-      scope.$digest();
-      httpBackend.flush();
-      
-      // Validation
-      expect(CustomerService.getCustomers).toHaveBeenCalled();
-      expect(result).toBeNull();
-    });
-
-  });
-
-  xdescribe('get tests', function () {
-    it('should get a customer with specified id', function () {
-      // Setup
-      httpBackend.whenGET('http://worktajm.arnellconsulting.dyndns.org:8080/api/api/customer/1').respond(_.clone(customers[0]));
-      spyOn(CustomerService, 'getCustomer').andCallThrough();
-      spyOn(scope, '$broadcast').andCallThrough();
-
-      // Test
-      var result = CustomerService.getCustomer(1);
-      scope.$digest();
-      httpBackend.flush();
-      
-      // Validation
-      expect(CustomerService.getCustomer).toHaveBeenCalledWith(1);
-      expect(result).toBeDefined();
-      expect(result.customerName).toBe(customers[0].customerName);
-    });
-
-    it('should try to get a customer with a specific id but get an error from backend', function () {
-      // Setup
-      httpBackend.whenGET('http://worktajm.arnellconsulting.dyndns.org:8080/api/api/customer').respond(401);
-      spyOn(CustomerService, 'getCustomers').andCallThrough();
-      spyOn(scope, '$broadcast').andCallThrough();
-
-      // Test
-      var result = CustomerService.getCustomers();
-      scope.$digest();
-      httpBackend.flush();
-      
-      // Validation
-      expect(CustomerService.getCustomer).toHaveBeenCalledWith(1);
-      expect(result).toBeNull();
-    });
-
-  });
-
-  xdescribe('update tests', function () {
-    it('should create a new customer', function () {
-    });
-
-    it('should try to create a new customer but get an error from backend', function () {
-    });
-  });
-
-  describe('detete tests', function () {
-    it('should create a new customer', function () {
-    });
-
-    it('should try to create a new customer but get an error from backend', function () {
+      expect(customerService.getCustomer).toHaveBeenCalledWith(customers[0]);
+      expect(failMessage).toBe('error');
     });
   });
 
