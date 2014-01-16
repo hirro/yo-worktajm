14beda98b3581b2318a84abedf334704c0a40a6e
Jim Arnell <jim@arnellconsulting.com>
2014-01-16 07:39:54 +0100

    Project modal validation [Finishes #63263480]

--- app/scripts/controllers/modals/projectModal.js
+++ app/scripts/controllers/modals/projectModal.js
@@ -7,6 +7,10 @@ angular.module('yoWorktajmApp').controller('ProjectModalCtrl', function ($scope,
       name: ''
     };
     $scope.ok = function () {
+      // FIXME
+      // Make sure project name is unique for the logged in person.
+      // If not, set the error status on the input
+      //$scope.projectForm.project.$setValidity('uniqueProjectPerUser', false);
       $modalInstance.close($scope.project);
     };
     $scope.cancel = function () {
