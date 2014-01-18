d9365a2091192e4827fb666ed5c8ca82dfeac6ff
Jim Arnell <jim@arnellconsulting.com>
2014-01-18 23:12:42 +0100

    Finally got require to work.

--- test/e2e/addProjectTest.js
+++ test/e2e/addProjectTest.js
@@ -27,35 +27,7 @@
 
 'use strict';
 
-require('./utilities.js');
-
-function generateUniqueId() {
-  var id = new Date().getTime().toString();
-  var random = Math.floor((Math.random()*100)+1).toString();
-  return id + random;
-}
-
-function login() {
-  browser.get('http://127.0.0.1:9000/');
-
-  // Enter the test user name, clear first to make sure suggested data is cleared
-  var usernameInput = element(by.model('username'));
-  usernameInput.clear();
-  usernameInput.sendKeys('jim@arnellconsulting.com');
-
-  // Enter the test user password, clear first to make sure suggested data is cleared
-  var passwordInput = element(by.model('password'));
-  passwordInput.clear();
-  passwordInput.sendKeys('password');
-
-  // Press the login
-  element(by.id('login')).click();
-
-  // We should now be logged in and transferred to the dashboard
-  // 
-  var firstName = element(by.binding('person.firstName'));
-  expect(firstName.getText()).toEqual('Jim Arnell');
-}
+var Utilities = require('./utilities.js');
 
 function addProject(project) {
 
@@ -132,12 +104,12 @@ function deleteProject(project) {
 
 describe('should add a project and then delete it', function() {
   it('should login successfully', function() {
-    login();
+    Utilities.login();
 
     var project = {
-      name: 'Project ' + generateUniqueId(),
+      name: 'Project ' + Utilities.generateUniqueId(),
       description: 'Description',
-      rate: generateUniqueId()
+      rate: 22
     };
     addProject(project);
     deleteProject(project);
--- test/e2e/utilities.js
+++ test/e2e/utilities.js
@@ -1,11 +1,30 @@
 'use strict';
 
 
-var Utilities =  {
-  generateUniqueId: function () {
-    var id = new Date().getTime().toString();
-    var random = Math.floor((Math.random()*100)+1).toString();
-    return id + random;
-  }
-};
+this.generateUniqueId = function () {
+  var id = new Date().getTime().toString();
+  var random = Math.floor((Math.random()*100)+1).toString();
+  return id + random;
+}
 
+this.login = function () {
+  browser.get('http://127.0.0.1:9000/');
+
+  // Enter the test user name, clear first to make sure suggested data is cleared
+  var usernameInput = element(by.model('username'));
+  usernameInput.clear();
+  usernameInput.sendKeys('jim@arnellconsulting.com');
+
+  // Enter the test user password, clear first to make sure suggested data is cleared
+  var passwordInput = element(by.model('password'));
+  passwordInput.clear();
+  passwordInput.sendKeys('password');
+
+  // Press the login
+  element(by.id('login')).click();
+
+  // We should now be logged in and transferred to the dashboard
+  // 
+  var firstName = element(by.binding('person.firstName'));
+  expect(firstName.getText()).toEqual('Jim Arnell');
+}
