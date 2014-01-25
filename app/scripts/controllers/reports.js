629074b08273508157abc2a4302b0552bd5ffabb
Jim Arnell <jim@arnellconsulting.com>
2014-01-25 21:12:28 +0100

    Report selection

--- app/scripts/controllers/reports.js
+++ app/scripts/controllers/reports.js
@@ -1,3 +1,30 @@
+/*
+  @licstart The following is the entire license notice for the 
+            JavaScript code in this page.
+  @source https://github.com/hirro/yo-worktajm
+
+  Copyright (C) 2013 Jim Arnell.
+
+  The JavaScript code in this page is free software: you can
+  redistribute it and/or modify it under the terms of the GNU
+  General Public License (GNU GPL) as published by the Free Software
+  Foundation, either version 3 of the License, or (at your option)
+  any later version.  The code is distributed WITHOUT ANY WARRANTY;
+  without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+
+  As additional permission under GNU GPL version 3 section 7, you
+  may distribute non-source (e.g., minimized or compacted) forms of
+  that code without the copy of the GNU GPL normally required by
+  section 4, provided you include this license notice and a URL
+  through which recipients can access the Corresponding Source.
+
+  @licend The above is the entire license notice
+          for the JavaScript code in this page.  
+*/
+
+/*globals  _ */
+
 'use strict';
 
 angular.module('yoWorktajmApp')
@@ -11,15 +38,18 @@ angular.module('yoWorktajmApp')
       { id: 'lastMonth',  name: 'Last month' }
     ];
     $scope.customers = [];
-    $scope.projects = TimerService.getProjects();
+    $scope.projects = _.each(TimerService.getProjects(), function (e) {
+      e.enabled = true;
+    });
     $scope.selectedTime = {};
     $scope.selectedCustomer = {};
-    $scope.selectedProjects = [];
+    $scope.selectedProjects = {};
 
     // Load the available customers
     CustomerService.list().then(function (result) {
       console.log('ReportsCtrl - Loaded customers from service');
-      $scope.customers = result;
+      var all = [{ id: 0, name: 'All'}];
+      $scope.customers = _.union(all, result);
     });
 
     $scope.generateReport = function () {
@@ -29,5 +59,9 @@ angular.module('yoWorktajmApp')
 
     $scope.changedCustomer = function () {
       console.log('Changed customer - updating filter?');
+    };
+
+    $scope.enabledProjects = function () {
+      return _.pluck(_.where($scope.projects, { enabled: true }), 'name');
     };
   });
