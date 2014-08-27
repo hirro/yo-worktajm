f253022dae7ebfd78132b04c00dbfafb6008e50d
Jim Arnell <jim@arnellconsulting.com>
2014-08-27 18:09:26 +0200

    Wrong karma browser

--- karma.conf.js
+++ karma.conf.js
@@ -97,7 +97,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['ChromeCanary'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
