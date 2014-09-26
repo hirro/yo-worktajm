698d07f62ca6dbd32e8101015a0835b572c79901
Jim Arnell <jim@arnellconsulting.com>
2014-09-26 08:24:45 +0200

    Trying out new theme. Site will look a bit bad until CSS styling issues are resolved

---
+++ client/app/landing/landing.controller.spec.js
@@ -0,0 +1,23 @@
+'use strict';
+
+describe('Controller: DashboardCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('worktajmApp'));
+  beforeEach(module('socketMock'));
+
+  var DashboardCtrl,
+      scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    DashboardCtrl = $controller('DashboardCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should attach a list of things to the scope', function () {
+    expect(scope.awesomeThings.length).toBe(0);
+  });
+});
