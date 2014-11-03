459240bd39d4ac7cb4643f5ae349e6a0fc6f9325
Jim Arnell <jim@arnellconsulting.com>
2014-11-03 18:18:47 +0100

    no message

--- client/app/reports/reports.controller.spec.js
+++ client/app/reports/reports.controller.spec.js
@@ -1,21 +1,4 @@
 'use strict';
 
 describe('Controller: ReportsCtrl', function () {
-
-  // load the controller's module
-  beforeEach(module('worktajmApp'));
-
-  var ReportsCtrl, scope;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    ReportsCtrl = $controller('ReportsCtrl', {
-      $scope: scope
-    });
-  }));
-
-  it('should ...', function () {
-    expect(1).toEqual(1);
-  });
 });
