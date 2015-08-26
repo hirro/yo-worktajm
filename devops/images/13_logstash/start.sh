db377b4607a9f1a844cc502c844f095979b8ee08
Jim Arnell <jim@arnellconsulting.com>
2015-08-26 21:57:08 +0200

    no message

--- devops/images/13_logstash/start.sh
+++ devops/images/13_logstash/start.sh
@@ -2,6 +2,5 @@
 docker run -d \
     --name logstash \
     --link elasticsearch:elasticsearch \
-	-v ~/worktajm.com/logstash:/etc/ssl/logstash/ \
     worktajm-logstash \
 	logstash --debug -f  /etc/logstash/logstash.conf
--- devops/images/92_logstash-forwarder/start.sh
+++ devops/images/92_logstash-forwarder/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 docker run --name logstash-forwarder \
-	--rm -ti \
-	-v ~/worktajm.com/logstash:/etc/ssl/logstash \
+	-d \
 	--link logstash:logstash \
 	--volumes-from nginx \
 	worktajm-logstash-forwarder
