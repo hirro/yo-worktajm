4f0971512935e029c2c7614204a73b93f1f6ce98
Jim Arnell <jim@arnellconsulting.com>
2015-08-27 15:47:42 +0200

    no message

--- devops/images/14_kibana/start.sh
+++ devops/images/14_kibana/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker run --name kibana -d \
-	-p 8080:80 \
+	-p 5601:5601 \
 	--link elasticsearch:elasticsearch \
+	-e ELASTICSEARCH_URL=http://elasticsearch:9200 \
 	worktajm-kibana
 
