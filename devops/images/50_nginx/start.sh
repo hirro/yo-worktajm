18ded7bb621da893a3584a99028a2f580a473fc9
Jim Arnell <jim@arnellconsulting.com>
2015-12-10 07:57:56 +0100

    no message

--- devops/images/50_nginx/start.sh
+++ devops/images/50_nginx/start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-docker run --name nginx -d \
-	--link node:node \
+docker run --name nginx \
+    -d \
 	-p 443:443 \
 	--privileged=true \
-	-v ~/worktajm.com/ssl:/etc/ssl/ \
+	-v /opt/docker/ssl:/etc/ssl/ \
+    -v /opt/docker/log:/var/log/nginx \
 	worktajm-nginx
 
